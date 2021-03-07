73015dbdcce38001fa8cad3b93340e5f8864a812
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-03-07 03:16:18 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -9,35 +9,32 @@ const Theme = {
 
 const menuListRef = document.querySelector('.js-menu');
 const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+const body = document.body;
 
 const markup = menuTemplate(menu);
 
 menuListRef.insertAdjacentHTML('beforeend', markup);
 
 themeSwitchToggle.addEventListener('change', e => {
-  localStorage.setItem('Theme', e.target.checked ? Theme.DARK : Theme.LIGHT);
   if (e.target.checked) {
-    document.querySelector('body').classList.add(Theme.DARK);
-    document.querySelector('body').classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('Theme', Theme.DARK);
+    body.classList.add(Theme.DARK);
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
   } else {
-    document.querySelector('body').classList.add(Theme.LIGHT);
-    document.querySelector('body').classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.LIGHT);
+    body.classList.replace(Theme.DARK, Theme.LIGHT);
   }
 });
 
 const currentTheme = localStorage.getItem('Theme');
 
-if (currentTheme) {
-  if (currentTheme === Theme.DARK) {
-    themeSwitchToggle.checked = true;
-    document.querySelector('body').classList.add(Theme.DARK);
-    document.querySelector('body').classList.replace(Theme.LIGHT, Theme.DARK);
-  }
-  if (currentTheme === Theme.LIGHT) {
-    themeSwitchToggle.checked = false;
-    document.querySelector('body').classList.add(Theme.LIGHT);
-    document.querySelector('body').classList.replace(Theme.DARK, Theme.LIGHT);
-  }
+if (currentTheme === Theme.DARK) {
+  themeSwitchToggle.checked = true;
+  body.classList.add(Theme.DARK);
+  body.classList.replace(Theme.LIGHT, Theme.DARK);
+} else {
+  themeSwitchToggle.checked = false;
+  body.classList.replace(Theme.DARK, Theme.LIGHT);
 }
 
 const btnClear = document.querySelector('.btn-clear');
